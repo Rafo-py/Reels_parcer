7d976f9a0ddd215ab1c8bc22526abbc3d33db3b1
Rafo-py <rafikadamyan2009@gmail.com>
2025-09-05 23:18:17 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ LOGIN = os.getenv("IG_LOGIN")
 PASSWORD = os.getenv("IG_PASSWORD")
 TOKEN = os.getenv("IG_TOKEN")
 
+WEBHOOK_URL = "https://reels-parcer.onrender.com/webhook"
+
 ADMIN_ID = '139740084'
+
 
