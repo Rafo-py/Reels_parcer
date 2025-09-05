a3962368d33a0e420ec33a158224bdb0cbb33368
Rafo-py <rafikadamyan2009@gmail.com>
2025-09-06 00:03:51 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
 INST_LOGIN = os.getenv("IG_LOGIN")       # логин для Instagram
-INST_PASSWORD = os.getenv("IG_PASSWORD")  # пароль для Instagram
+INST_PASS = os.getenv("IG_PASSWORD")  # пароль для Instagram
 TOKEN = os.getenv("IG_TOKEN")
 
 WEBHOOK_URL = "https://reels-parcer.onrender.com/webhook"
 
 ADMIN_ID = '139740084'
+
 
 
 
