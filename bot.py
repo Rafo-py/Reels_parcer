e0916af05d8587b556113fef17722601212fa60b
Rafo-py <rafikadamyan2009@gmail.com>
2025-09-05 23:11:44 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,19 +5,18 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from config import TOKEN, ADMIN_ID
 from database.db import async_main
 from handlers import start, admin_commands, sender, check_username, reels
-from Server import start_web_server  # импортируем функцию из server.py
+from Server import start_web_server
 
-# Настройка логирования
+# Логирование
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 )
 
-# Создание бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Подключение роутеров
+# Подключаем роутеры
 dp.include_routers(
     start.router,
     admin_commands.router,
@@ -28,20 +27,17 @@ dp.include_routers(
 
 async def main():
     try:
-        # Запускаем web-сервер для Render в фоне
-        server_task = asyncio.create_task(start_web_server())
-
-        # Инициализация базы данных
+        # БД
         await async_main()
         engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
         async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-        # Запуск бота
-        await dp.start_polling(bot)
+        # Запуск веб-сервера + webhook
+        await start_web_server()
     except Exception as e:
         logging.error(f"Ошибка при запуске бота или сервера: {e}")
     finally:
-        await bot.session.close()  # корректное завершение сессии
+        await bot.session.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
